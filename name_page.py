512067aad23e779103ef78d3b9392c797cfa1bc3
LuckyTungariya <luckytungariya412@gmail.com>
2025-01-12 18:48:35 +0530

    NamePage ready with pics_folder uploaded

---
+++ name_page.py
@@ -0,0 +1,35 @@
+import customtkinter as ctk
+import tkinter as tk
+from PIL import Image,ImageTk
+
+class NamePage(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.width = self.winfo_screenwidth()
+        self.height = self.winfo_screenheight()
+        self.geometry('{}x{}+{}+{}'.format(self.width,self.height,0,0))
+        self.title("NamePage")
+        self.config(bg="#ffbe0b")
+
+        self.bind("<f>",self.toggle_fullscreen)
+
+        self.middle_frame = ctk.CTkFrame(self,width = 550,height = 500,fg_color="#001d3d",bg_color="#001d3d",corner_radius=20)
+        self.middle_frame.place(x = 350,y = 120)
+
+        self.name_label = ctk.CTkLabel(self.middle_frame,text="Name",width=150,height=50,corner_radius=15,fg_color="#ffffff",text_color="#000000",font = ctk.CTkFont(family = "Cursive",size = 20,weight = "bold"))
+        self.name_label.place(x = 200,y = 100)
+
+        self.name_value = ctk.CTkEntry(self.middle_frame,height = 50,width=300,corner_radius=20,placeholder_text="Enter your name:",placeholder_text_color="#000000",font = ctk.CTkFont(family="Helvetica",size = 20,weight="normal"))
+        self.name_value.place(x = 130,y = 170)
+
+        self.play_img = ctk.CTkImage(dark_image=Image.open(r'C:\\Users\\lucky\\OneDrive\\Desktop\\SYCS SEM3\\QuizScienceMela\\Science_Mela\\pics_folder\\play-button-arrowhead.png'),size=(30,30))
+
+        self.play_button = ctk.CTkButton(self.middle_frame,width = 150,image=self.play_img,height=50,corner_radius=20,text="Play",text_color="#000000",fg_color="#ffbe0b",hover_color="#FFD54F",font = ctk.CTkFont(family = "Helvetica",size = 20,weight="bold"))
+        self.play_button.place(x = 200,y = 270)
+
+    def toggle_fullscreen(self,event = None):
+        self.overrideredirect(True)
+
+namepage = NamePage()
+namepage.mainloop()
+                      
